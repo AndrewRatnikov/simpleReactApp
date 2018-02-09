6fcdacbea8aafefc5e95efc1f1d20cf386a4a3dc
Andrew Ratnikov <andrew.ratnikov@corp.kismia.com>
2018-02-09 18:27:30 +0200

    update eslint

--- src/components/placeholder.js
+++ src/components/placeholder.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import PropsTypes from 'prop-types';
 
 export default function ( Component, Placeholder ) {
     return class extends React.Component {
+        static propsTypes = {
+            breed: PropsTypes.object,
+            breeds: PropsTypes.object
+        }
+
         needPlaceholder () {
             if ( this.props.breed ) return true;
             else if ( this.props.breeds ) return true;
